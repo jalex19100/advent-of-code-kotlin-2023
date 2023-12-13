2fab22b96f04ede1a7b1742e0456091809604cb4
jason_alexander <jason.alexander@mheducation.com>
2023-12-12 22:53:15 -0500

    day08

--- src/Day07.kt
+++ src/Day07.kt
@@ -16,14 +16,13 @@ fun main() {
 
     val cardIndex1 = listOf('1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
     val cardIndex2 = listOf('J', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
-w
+
     data class Hand(val cards: String, val groups: Map<Char, Int>, val bid: Int, val part2: Boolean)
 
     fun Hand(input: String, part2: Boolean = false): Hand {
         val (cards, bid) = input.split(" ")
         val groups = if (part2 && cards.contains('J')) {
-            // TODO :-o hacked to pieces
-            val highCard = if (cards.equals("JJJJJ")) 'A'
+            val highCard = if (cards == "JJJJJ") 'A'
             else cards.replace("J", "").toList().sortedWith(compareBy<Char> { cardIndex2.indexOf(it) }).groupingBy { it }.eachCount()
                 .maxBy { it.value }.key
             cards.replace('J', highCard).toList().groupingBy { it }.eachCount()
@@ -49,14 +48,12 @@ w
 
     fun part1(input: List<String>): Int {
         val hands = input.map { Hand(it) }.sortedWith(handComparator)
-        //        println("$hands")
         val winnings = hands.foldIndexed(0) { idx, acc, hand -> acc + hand.bid * (idx + 1) }
         return winnings
     }
 
     fun part2(input: List<String>): Int {
         val hands = input.map { Hand(it, true) }.sortedWith(handComparator)
-        //        println("$hands")
         val winnings = hands.foldIndexed(0) { idx, acc, hand -> acc + hand.bid * (idx + 1) }
         return winnings
     }
--- src/Day08.kt
+++ src/Day08.kt
@@ -1,5 +1,3 @@
-import sun.nio.ch.Net
-
 fun main() {
 
     data class Node(val element: String, val left: String, val right: String)
@@ -13,49 +11,43 @@ fun main() {
     fun Network(input: List<String>): Network {
         val regex = Regex("(\\w+)")
         val nodes = input.associate {
-            val groupValues = regex.findAll(it).toList().map { it.groupValues[1] }
+            val groupValues = regex.findAll(it).toList().map { match -> match.groupValues[1] }
             groupValues[0] to Node(groupValues[0], groupValues[1], groupValues[2])
         }
         return Network(nodes)
     }
 
-    fun part1(input: List<String>): Int {
-        val instructions = input[0].repeat(150).iterator()
-        val network = Network(input.drop(2))
-        fun Network.getSteps1(): Sequence<Node> = generateSequence(seedFunction = { this.nodes["AAA"] }, nextFunction = {
-            if (it.element == "ZZZ") null else {
+    fun Network.getSteps(instructions: CharIterator, startElement: String = "AAA", endSuffix: String = "ZZZ"): Sequence<Node> =
+        generateSequence(seedFunction = { this.nodes[startElement] }, nextFunction = {
+            if (it.element.endsWith(endSuffix)) null else {
                 this.nodes[it.turn(instructions.next())]
             }
         })
 
-        return network.getSteps1().count() - 1
-    }
-
-    fun part2(input: List<String>): Int {
-        val instructions = input[0].repeat(500000 ).iterator()
+    fun part1(input: List<String>): Int {
+        val instructions = input[0].repeat(150).iterator()
         val network = Network(input.drop(2))
-        fun Network.getSteps2(): Sequence<Collection<Node>> = generateSequence(
-            seedFunction = { this.nodes.filter { (k, _) -> k.endsWith("A") }.values },
-            nextFunction = {
-                val nextTurn = instructions.next()
-                if (it.all { node -> node.element.endsWith("Z") }) null
-                else this.nodes.filter { (_, v) ->
-                    it.map { node -> this.nodes[node.turn(nextTurn)] }.contains(v)
-                }.values
-            })
-
-        val moves = network.getSteps2()
-        return moves.count() - 1
+        return network.getSteps(instructions, "AAA", "ZZZ").count() - 1
     }
 
-    val testInput = readInput(                                                                                       ju"Day08_test")
+    fun part2(input: List<String>): Long {
+        val instructions = input[0].repeat(500).iterator()
+        val network = Network(input.drop(2))
+        val startingNodes = network.nodes.filter { (k, _) -> k.endsWith("A") }.values
+        val stepCountsByNode = startingNodes.associate { node ->
+            node.element to network.getSteps(instructions, node.element, "Z").count() - 1
+        }.values
+        return stepCountsByNode.fold(stepCountsByNode.max().toLong()) { result, number -> lcm(result.toLong(), number.toLong()) }
+    }
+
+    val testInput = readInput("Day08_test")
     val testInput2 = readInput("Day08_test2")
     part1(testInput).println()
     check(part1(testInput) == 6)
     part2(testInput2).println()
-    check(part2(testInput2) == 6)
+    check(part2(testInput2) == 6L)
 
     val input = readInput("Day08")
     part1(input).println() // 18727
-    part2(input).println()
+    part2(input).println() // 18024643846273
 }
